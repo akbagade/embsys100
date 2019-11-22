1be61cfda6efd0c2d2362a91fb7830c3b422484f
akbagade <52082988+akbagade@users.noreply.github.com>
2019-11-21 18:42:59 -0600

    Create function.c

---
+++ Assignment 4/problem 2/function.c
@@ -0,0 +1,18 @@
+
+int result = 0;
+int sum(int var0, int var1, int var2, int var3, int var4)
+{
+    
+ 
+     
+     int sum = var0 + var1 + var2 + var3 + var4;
+     return sum;
+}
+
+
+
+int main()
+{
+  result = sum(3,4,2,6,2);
+  return 0;
+}
