9ec0809bc44590d2c586c3961bf452cb11bc124f
akbagade <52082988+akbagade@users.noreply.github.com>
2019-11-25 21:05:04 -0600

    Create Stack.h

---
+++ Assignment 4/Stack.h
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+void stackstart();
+bool stackpush(int element);
+bool stackpush(int element);
+bool stackpush(int element);
+bool stackpush(int element);
+bool stackPop(int* element);
+bool stackPop(int* element);
+bool stackPop(int* element);
+bool stackempty();
+bool stackfull();
